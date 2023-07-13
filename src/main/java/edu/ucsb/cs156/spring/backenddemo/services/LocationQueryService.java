d48bf10b3026331cde3cd9247635b498f500dcc9
Hao Wu <hao_wu@csil.cs.ucsb.edu>
2023-07-13 14:56:56 -0700

    hw - first draft of location

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,12 +1,16 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class LocationQueryService {
 
@@ -19,6 +23,8 @@ public class LocationQueryService {
     public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search/{location}?format=json";
 
     public String getJSON(String location) throws HttpClientErrorException {
+        log.info("location={}", location);
+        Map<String, String> uriVariables = Map.of("location", location);
         return "";
     }
 }
